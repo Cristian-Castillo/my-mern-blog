79b80d037da6f8ae6977d0157ba7e7f38f21f740
ccarri11@ucsc.edu <ccarri11@ucsc.edu>
2022-01-04 10:50:22 -0800

    V25

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -25,7 +25,7 @@ export default function Write() {
       data.append("file", file);
       newPost.photo = filename;
       try {
-        await axios.post("/upload", data);
+        await axios.post("https://blog-n-mern.herokuapp.com/api/upload", data);
       } catch (err) {}
     }
     try {
