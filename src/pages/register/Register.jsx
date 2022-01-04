a5dc17765ec4876f2bb2c987586cf68decdcd1c5
ccarri11@ucsc.edu <ccarri11@ucsc.edu>
2022-01-04 09:54:46 -0800

    v 9.0

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -18,7 +18,7 @@ export default function Register() {
         email,
         password,
       });
-      res.data && window.location.replace("https://confident-yalow-88511d.netlify.app/login");
+      res.data && window.location.replace("/login");
     } catch (err) {
       setError(true);
     }
